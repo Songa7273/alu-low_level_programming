91a698ade2ebb462c8b547a5c1956e291ebd549c
Songa7273 <k.gaju@alustudent.com>
2026-02-27 05:20:09 -0800

    2-print_dog.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,19 +2,19 @@
 #define DOG_H
 
 /**
- * struct dog - structure representing a dog
+ * struct dog - structure for a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
-#endif
+#endif /* DOG_H */
